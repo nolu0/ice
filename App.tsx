327b73209f22a0ae9c43747f1d9c2dfe5368c725
nolu0 <163901292+nolu0@users.noreply.github.com>
2024-09-06 17:36:39 +0200

    commit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { FlatList, SafeAreaView, StyleSheet, Text, View, Image, TextInput, TouchableHighlight} from 'react-native';
-import {Picker} from"@react-native-picker/picker";
+import {Picker} from "@react-native-picker/picker";
 import {WorkoutDetails} from './types';
 
 
@@ -27,52 +27,71 @@ export default function App() {
       const newWorkout: WorkoutDetails ={  /*creates new workout objects*/
 workOut_Name: workOutName, /*user input for the workout added to the new object*/ 
 duration: parseInt(duration), /*thes structure of the workoutDetails object declares duration as numeric*/ 
-<excercise_Type:excerciseType, /*users selected value  for exercise type from the picker is added to new object*/ 
+excercise_Type:excerciseType, /*users selected value  for exercise type from the picker is added to new object*/ 
 calories: parseInt(calories)}; /* the structure of the wotkoutDEtails objrct declares calories as numeric*/ 
 
 
-setWorkouts([..workouts, newWorkouts]); //updating the workout list with the newly added workout
-setTotalCalories(totalCalories, newWorkout, calories); // updates the totalcalories with the new
+setWorkouts([...workouts, newWorkout]); //updating the workout list with the newly added workout
+setTotalCalories(totalCalories, newWorkout.calories); // updates the totalcalories with the new
 setWorkoutsName('');
 setDuration('');
 setType('None');
 setCalories('');
     }
   }
-  const totalworkouts = workouts.length; //holds the total amount of workouts in the list
+  const totalworkouts =workouts.length; //holds the total amount of workouts in the 
 
+  renderItem={({item}) => (
 
+  <View style={Style.container}>
+  <Text style={StyleSheet.workoutName}>workOut_Name: {Item.workout_Name</Text>
+  <Text style={StyleSheet.otherDetails}>Duration: {ClipboardItem.duration}</Text>
+</View>
+  )}
+  />
+  </View>
 
-  return (
+return  (
+  
+  <SafeAreaView styles={style.itemcontainer}>
+  <View style={styles.headingContainer}
+  <Text style={styles.trackName}> FITNESS TRACKER</Text>
+</View>
 
+<View style ={Styles.listview}
+<FlatList
+style = {Style.liststyle}
+data = {workouts}
+keyExtractor={(item, index) => index.toString()}
+<Picker
+selectedvalue={excerciseType} // the current value selected in the picker (dropdown menu.)
+onValuechange={(itemValue)} => setType(itemValue) //Event handler called when a user selects a new value from the picker
+// it updates the state varaible 'exerciseType' with the selected value.
 
-    <SafeAreaView style={styles.container}>
-      <View style={styles.headingContainer}
-      <Text style={styles.trackName}> FITNESS TRACKER</Text>
-    </View>
+style={styles.input}>
 
-    <View style={StyleSheet.summarycontainer}>
-      <Text style={StyleSheet.summaryHeading}>TODAY'S SUMMARY:</Text>
-      <View style={StyleSheet.summaryContent}>
-        </View>
+  (ExcerciseType.map((excerciseType) => (
+    <Picker.Item Label={excerciseType} value={excerciseType} key={excerciseType}/>
+  ))}
+</Picker>
 
-        <Text style={StyleSheet.summaryText}>Total Workouts: {totalworkouts}</Text>
-        <Text style={StyleSheet.summaryText}>Total calories Burned</Text>
-        </View>
-        <Image source={requires(./Images)}
-        </View>
-        </View>
+<View style={Style.userInputView}
+<TextInput
+style={StyleSheet.input}
+placeholder='workout name'
+value={workoutName}
+onChangeText={setWorkoutName}
+</TextInput>
 
-        <View style ={StyleSheet.listView}>
-          <FlatList
-          style ={Styles.listStles}
-          data= {workouts}
-          keyExtractor={(item, index)} => index.toString()}
-          renderItem={(item)} => (
-            <View style={StyleSheet.container}>
-              <Text style={StyleSheet.WorkoutName}>WORKOUT NAME: {Item.workout_name</Text>
-              <Text style={StyleSheet.otherDetails}> Duration {Item.duration}</Text>
-        </View>
+<TextInput
+style ={StyleSheet.input}
+placeholder='duration'(min)
+value ={duration}
+onChangeText={setDuration}
+inputMode='numeric'>
+</TextInput>
 
-
-        
+      <View style={StyleSheet.container}>
+        <Text style={StyleSheet.WorkoutName}>WORKOUT NAME: {Item.workout_name}</Text>
+        <Text style={StyleSheet.otherDetails}> Duration {Item.duration}</Text>
+      
